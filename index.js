fabbb578735583aa1972cdb74d6f92172f1575fd
dumau144 <phellipe.rds144@gmail.com>
2022-07-03 12:05:52 -0300

    line SDF

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 import { ImageJS } from "./lib/imagejs/imagejs.js";
-import { SDFsquare } from "./lib/sdf/sdf.js";
+import { length2 } from "./lib/sdf/mathpls.js";
+import { lineSegment, SDFsquare } from "./lib/sdf/sdf.js";
+
+const mouse = {
+  x: 0,
+  y: 0,
+  btn:[],
+  move(event) {
+    this.x = event.offsetX
+    this.y = event.offsetY
+  }
+}
+
+const p1 = {x: -1 + 2 * Math.random(), y: -1 + 2 * Math.random()};
+const p2 = {x: -1 + 2 * Math.random(), y: -1 + 2 * Math.random()};
+
+const lp1 = {x: 0.5, y: 0.8};
+const lp2 = {x: -0.2, y: 0};
+addEventListener('mousemove', event => mouse.move(event));
+addEventListener('mousedown', event => mouse.btn[event.button] = true);
+addEventListener('mouseup', event => mouse.btn[event.button] = false);
 
 const squareSize = 0.25;
+
+const triWave = (x) => Math.abs(Math.abs(x % 2) - 1);
 
 {
   const canvas = document.getElementsByTagName('canvas')[0];
   const ctx = canvas.getContext('2d');
+
+  const resizeCanvas = () => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  }
+
+  resizeCanvas();
+
+  window.addEventListener('resize', resizeCanvas);
 
   const imagejs = new ImageJS(canvas.width, canvas.height);
 
@@ -14,46 +45,112 @@ const squareSize = 0.25;
   const mainImage = (fragColor, fragCoord) => {
     const t = performance.now() / 1000;
 
-    const x = fragCoord.x / canvas.width * aspectRatio - 1;
-    const y = fragCoord.y / canvas.height - 0.5;
+    const x = 2 * (fragCoord.x / canvas.width - 0.5) * aspectRatio;
+    const y = 2 * (fragCoord.y / canvas.height- 0.5);
 
-    const square = 0.5 + 0.5 * Math.cos(SDFsquare({ x: x, y: y}, squareSize) * Math.PI * 40 + t);
+    const square = 0.5 + 0.5 * Math.cos(SDFsquare({ x: x, y: y}, squareSize) * Math.PI * 20 + t);
 
-    fragColor(square, square, square, 1.0);
+    fragColor(0, 0, 0, 1.0);
   };
 
   const loop = () => {
-    imagejs.draw(mainImage);
-    imagejs.putImage(ctx);
+    //imagejs.draw(mainImage);
+    //imagejs.putImage(ctx);
+
+    const mx = 2 * (mouse.x / canvas.width - 0.5) * aspectRatio;
+    const my = 2 * (mouse.y / canvas.height- 0.5);
+
+    if(mouse.btn[0]) {p1.x = mx; p1.y = my};
+    if(mouse.btn[1]) {p2.x = mx; p2.y = my};
+
+    const square = SDFsquare({ x: p2.x, y: p2.y}, squareSize);
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.translate(canvas.width / 2.0, canvas.height / 2.0);
+    ctx.scale(canvas.height / 2.0, canvas.height / 2.0);
+
+    ctx.lineWidth = 0.5 / canvas.height;
+
+    ctx.strokeRect(-squareSize, -squareSize, squareSize * 2, squareSize * 2);
+
+    const deg = Math.atan2(p1.x - p2.x, p1.y - p2.y);
+
+    const lookAt = {
+      x: Math.sin(deg),
+      y: Math.cos(deg),
+    }
+
+    ctx.fillStyle = '#00000040'
+
+    ctx.beginPath();
+    ctx.arc(p1.x, p1.y , 0.01, 0, 2 * Math.PI);
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc(p2.x, p2.y , 0.01, 0, 2 * Math.PI);
+    ctx.fill();
+
+    //Ray marching
+
+    let rx = p2.x;
+    let ry = p2.y;
+
+    ctx.beginPath();
+    ctx.moveTo(lp1.x, lp1.y);
+    ctx.lineTo(lp2.x, lp2.y);
+    ctx.stroke();
+
+    for(let i = 0; i < 255; i++) {
+      const len = length2(p2.x - rx,p2.y - ry);
+
+      const square = SDFsquare({x: rx, y: ry}, 0.25);
+      const line = lineSegment({x: rx, y: ry}, lp1, lp2)
+
+      const scene = Math.min(
+        square, 
+        line
+      );
+
+      if(scene < 0.0001 || len > 8) break
+
+      ctx.beginPath();
+      ctx.arc(rx, ry, scene, 0, 2 * Math.PI);
+      ctx.stroke();
+
+      ctx.beginPath();
+      ctx.moveTo(rx, ry);
+
+      rx = rx + Math.sin(deg) * scene;
+      ry = ry + Math.cos(deg) * scene;
+
+      ctx.lineTo(rx, ry);
+      ctx.stroke();
+    };
+
+    ctx.resetTransform();
+
+
+    ctx.fillStyle = '#000000'
+    ctx.font = "30px Arial";
+    ctx.fillText('depth: '+length2(rx - p2.x, ry - p2.y), 10, 100);
+
     requestAnimationFrame(loop);
   };
 
   loop(); 
 };
 
-{
+/*{
   const canvas = document.getElementsByTagName('canvas')[1];
   const ctx = canvas.getContext('2d');
 
-  const ws = canvas.width / 2;
-  const hs = canvas.height / 2;
-
-  const draw = () => {
-    ctx.lineWidth = 1 / canvas.height;
-    ctx.strokeRect(-squareSize, -squareSize, squareSize * 2, squareSize * 2);
-    ctx.beginPath();
-    ctx.arc(0, 0, 1, 0, 2 * Math.PI, false);
-    ctx.stroke();
-  };
-
   const loop = () => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.translate(ws, hs);
-    ctx.scale(canvas.height, canvas.height);
-    draw()
-    ctx.resetTransform();
-    requestAnimationFrame(loop);
+    for(let i = 0; i < canvas.width; i++) {
+      ctx.fillStyle = `hsl(0deg 0% ${100 * Math.random()}%)`
+      ctx.fillRect(i, 0, 1, canvas.height)
+    }
+    //requestAnimationFrame(loop);
   };
 
   loop();
-};
+};*/
